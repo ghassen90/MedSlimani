10612bbc240e2e7891b3f9dbe691aefe25315029
gaston90 <alamri.ghassen@gmail.com>
2024-11-23 23:07:32 +0100

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
-export const environment1 = {
-  production: true,
-  baseUrl : "http://51.68.172.30:3000"
-};
 export const environment = {
+  production: true,
+  baseUrl : "http://51.222.156.45:3000"
+};
+export const environment1 = {
   production: true,
   baseUrl : "http://localhost:3000"
 };
